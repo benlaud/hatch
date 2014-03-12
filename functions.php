38eb2bd0db9ea4d81686975660a4068bb5585981
Aaron Ware <aware@linchpinagency.com>
2014-03-12 10:42:53 -0400

    Cleanup of the base Added in a new grunt dependence to match dependencies. Updated the grunt file to also watch for foundation-init changes Removed a lot of the FoundationPress specific methods that didn't need to be there

--- functions.php
+++ functions.php
@@ -1,32 +1,18 @@
 <?php
-/*
-Author: Ole Fredrik Lie
-URL: http://olefredrik.com
-*/
+/**
+ * Linchpin
+ *
+ * FoundationPress
+ * @author Aaron Ware
+ *
+ * @category    design
+ * @package     FoundationPress
+ * @copyright   Copyright (c) 2013 Linchpin (Digital Octane, LLC). (http://www.linchpinagency.com)
+ * @created     3/10/14
+ */
 
+include_once( 'includes/classes/FoundationPress.php' );
+include_once( 'includes/classes/Foundation/Foundation.php' );
+include_once( 'includes/classes/Foundation/Foundation_Nav_Walker.php' );
 
-// Various clean up functions
-require_once('library/cleanup.php'); 
-
-// Required for Foundation to work properly
-require_once('library/foundation.php');
-
-// Register all navigation menus
-require_once('library/navigation.php');
-
-// Add menu walker
-require_once('library/menu-walker.php');
-
-// Create widget areas in sidebar and footer
-require_once('library/widget-areas.php');
-
-// Return entry meta information for posts
-require_once('library/entry-meta.php');
-
-// Enqueue scripts
-require_once('library/enqueue-scripts.php');
-
-// Add theme support
-require_once('library/theme-support.php');
-
-?>
+$foundationPress = new FoundationPress();
