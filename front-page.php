8f9b7ec5dfbe2797c63cb46f4d6c0324e5b8607b
Jason Narciso <jnarciso@linchpinagency.com>
2014-07-03 08:35:47 -0400

    Added a left sidebar template, and some widgets to front-page.php, finished comments styling.

---
+++ front-page.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+
+	<div class="row cta">
+		<?php dynamic_sidebar("home-widgets"); ?>
+	</div>
+
+<?php get_footer();
--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 <aside id="sidebar" class="small-12 large-4 columns">
 	<?php do_action('foundationPress_before_sidebar'); ?>
-	<?php dynamic_sidebar("home-widgets"); ?>
+	<?php dynamic_sidebar("page-widgets"); ?>
 	<?php do_action('foundationPress_after_sidebar'); ?>
 </aside>
