9597dc75511aea3c297c2ccaef40996b1ffc2b9e
Aaron Ware <aware@linchpinagency.com>
2014-10-07 13:26:14 -0400

    Massive change to the structure. Probably broke EVERYTHING. STAY TUNED!

---
+++ root/functions.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ *
+ * Include all of our needed Classes and scripts
+ * Originally based on the work done by Ole Fredrik Lie (http://olefredrik.com)
+ * Forked by Linchpin
+ *
+ * We've added in a ton of functionality we utilize on a daily basis.
+ * It's not necessarily ment to have a TON of options and controls.
+ * Just a clean base for our build outs.
+ *
+ */
+
+// Launchpad Classes
+
+include_once('includes/Linchpin/Launchpad.php');
+
+// Foundation Classes
+
+include_once('includes/Foundation/Foundation.php');
+
+// Theme Class
+
+include_once('includes/Theme.php');
+
+/**
+ *	Instantiate our classes.
+ */
+
+$theme	= new Theme();
+$cpt 	= new CustomPostTypes();
---
+++ root/includes/Linchpin/Launchpad.php
@@ -0,0 +1,20 @@
+<?php
+
+$linchpin_classes_dir = get_template_directory() . '/includes/Linchpin/';
+
+include_once( $linchpin_classes_dir . 'LaunchpadOptions.php' );
+include_once( $linchpin_classes_dir . 'LaunchpadCustomHeader.php' );
+include_once( $linchpin_classes_dir . 'LaunchpadActivate.php' );
+include_once( $linchpin_classes_dir . 'LaunchpadUtilities.php' );
+include_once( $linchpin_classes_dir . 'LaunchpadMenu.php' );
+include_once( $linchpin_classes_dir . 'CustomPostTypes.php' );
+
+class Launchpad {
+	function __construct() {
+		$launchpad_activate 	   = new LaunchpadActivate();
+		$launchpad_custom_header   = new LaunchpadCustomHeader();
+		$launchpad_option_controls = new LaunchpadOptions();
+		$launchpad_utilities	   = new LaunchpadUtilities();
+		$launchpad_menu			   = new LaunchpadMenu();
+	}
+}
