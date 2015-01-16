bde3c281ae3fb3f7034b284818ee355dbe6d065e
Max Morgan <mmorgancreative@gmail.com>
2015-01-16 09:29:00 -0500

    Setting this to match up with the mods on DLX

--- root/wp-content/themes/theme/Gruntfile.js
+++ root/wp-content/themes/theme/Gruntfile.js
@@ -105,12 +105,6 @@ module.exports = function(grunt) {
       }
     }
   });
-  
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('build', ['sass']);
   grunt.registerTask('default', ['copy', 'uglify', 'concat', 'watch']);
