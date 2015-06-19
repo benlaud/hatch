fcb62a7319e2964274356f1e285eedf627ce7ab4
Aaron Ware <aware@linchpinagency.com>
2015-06-19 14:45:41 -0400

    Update to the template.js to add new options for wp-content/themes directories Reformat of the Search template Get rid of woo commerce template by default

--- root/wp-content/themes/theme/search.php
+++ root/wp-content/themes/theme/search.php
@@ -11,39 +11,46 @@
 ?>
 <?php get_header(); ?>
 
-<div class="row">
+	<div class="row">
 	<div class="small-12 large-8 columns" role="main">
 
-		<h2><?php esc_html_e( 'Search Results for', '{%= text_domain %}' ); ?> "<?php esc_html_e( get_search_query() ); ?>"</h2>
+		<h2><?php esc_html_e( 'Search Results for', '{%= text_domain %}' ); ?>
+			"<?php esc_html_e( get_search_query() ); ?>"</h2>
 
-        <?php
-        /** This action is documented in includes/Linchpin/hatch-hooks.php */
-        do_action( 'hatch_content_before' ); ?>
+		<?php
+		/** This action is documented in includes/Linchpin/hatch-hooks.php */
+		do_action( 'hatch_content_before' ); ?>
 
-        <?php if ( have_posts() ) : ?>
+		<?php if ( have_posts() ) : ?>
 
-            <?php
-            /** This action is documented in includes/Linchpin/hatch-hooks.php */
-            do_action( 'hatch_loop_before' ); ?>
+			<?php
+			/** This action is documented in includes/Linchpin/hatch-hooks.php */
+			do_action( 'hatch_loop_before' ); ?>
 
-            <?php while ( have_posts() ) : the_post(); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 
-                <?php get_template_part( 'content', get_post_format() ); ?>
+				<?php get_template_part( 'content', get_post_format() ); ?>
 
-            <?php endwhile; ?>
+			<?php endwhile; ?>
 
-        <?php else : ?>
+			<?php
+			/** This action is documented in includes/Linchpin/hatch-hooks.php */
+			do_action( 'hatch_loop_after' ); ?>
 
-            <?php get_template_part( 'content', 'none' ); ?>
+		<?php else : ?>
 
-        <?php endif;?>
+			<?php get_template_part( 'content', 'none' ); ?>
 
-        <?php do_action( 'hatch_content_after' ); ?>
+		<?php endif; ?>
 
-        <?php get_template_part( 'includes/partials/pagination' ); ?>
+		<?php
+		/** This action is documented in includes/Linchpin/hatch-hooks.php */
+		do_action( 'hatch_content_after' ); ?>
+
+		<?php get_template_part( 'includes/partials/pagination' ); ?>
 
 	</div>
 
-	<?php get_sidebar(); ?>
+<?php get_sidebar(); ?>
 
 <?php get_footer();
--- template.js
+++ template.js
@@ -1,7 +1,7 @@
 /**
  * hatch grunt scaffold
  * heavily based on the following project
- * Copyright (c) 2014 Aaron Ware, Linchpin
+ * Copyright (c) 2015 Aaron Aaron Ware, Maxwell Morgan, Jonathan Desrosiers, Linchpin
  * Updated by aware on 10/7/14.
  *
  * Cleaned up and removed some unneeded elements
@@ -9,7 +9,7 @@
  * grunt-wp-theme
  * https://github.com/10up/grunt-wp-theme
  *
- * Copyright (c) 2013 Eric Mann, 10up, Aaron Ware, Linchpin
+ * Copyright (c) 2013 Eric Mann, 10up
  * Licensed under the MIT License
  *
  */
@@ -17,13 +17,24 @@
 'use strict';
 
 // Basic template description
-exports.description = 'Create a WordPress theme.';
+exports.description = 'Create a WordPress theme based on Foundation.';
 
 // Template-specific notes to be displayed before question prompts.
-exports.notes = '';
+exports.notes = '_Project Classes and Namespaces_ should not contain "Hatch", "Linchpin" or "WordPress" and ' +
+    'should be a unique ID not already in use in wordpress.org plugins or themes directory. _Theme ' +
+    'name_ should be a human-readable title, and doesn\'t need to contain ' +
+    'the word "WordPress", although it may. For example, a theme named "Awesome ' +
+    'Theme" might have the name space "awesome-theme".' +
+    '\n\n'+
+    'For more information, please see the WordPress coding standards';
 
-// Template-specific notes to be displayed after the question prompts.
-exports.after = '';
+exports.after = 'You should now _cd to_your_base_path' +
+    '_ and install project dependencies with _npm ' +
+    'install && bower install_.  After that, you may execute project tasks with _grunt_. For ' +
+    'more information about installing and configuring Grunt, please see ' +
+    'the Getting Started guide:' +
+    '\n\n' +
+    'http://gruntjs.com/getting-started';
 
 // Any existing file or directory matching this wildcard will cause a warning.
 exports.warnOn = '*';
@@ -32,82 +43,120 @@ exports.warnOn = '*';
 exports.template = function( grunt, init, done ) {
     init.process( {}, [
         // Prompt for these values.
-        init.prompt( 'title', 'WP Theme / Client Name' ),
+        {
+            name   : 'title',
+            message: 'WordPress Theme Name / Client Name',
+            default: 'My New Theme'
+        },
         {
             name   : 'php_class_name',
             message: 'PHP Class name used to encapsulate our code',
-            default: 'HatchTheme'
+            default: 'MyThemeClass'
         },
         {
             name   : 'prefix',
-            message: 'PHP function prefix (alpha and underscore characters only)',
+            message: 'PHP and JavaScript function prefix (alpha and underscore characters only)',
             default: 'lp_'
         },
-        init.prompt( 'base_version', '0.1' ),
-        init.prompt( 'description', 'A brief description about the theme/client.' ),
+        {
+            name   : 'create_base_directories',
+            message: 'Would you like to create "/wp-content/themes/" parent directories?',
+            default: 'Y/n'
+        },
+        init.prompt( 'base_version', '0.1.0' ),
+        init.prompt( 'description', 'A brief description about the theme or client.' ),
         init.prompt( 'homepage', 'http://linchpin.agency' ),
         init.prompt( 'author_name' ),
         init.prompt( 'author_email' ),
         init.prompt( 'author_url', 'http://linchpin.agency' ),
-        init.prompt( 'text_domain', 'hatch' )
+        {
+            name   : 'text_domain',
+            message: 'Text domain used for localization',
+            default: 'hatch'
+        }
     ], function( err, props ) {
         props.keywords = [];
         props.version = '0.1.0';
         props.devDependencies = {
             "grunt": "~0.4.1",
-		    "grunt-contrib-clean": "~0.6.0",
-		    "grunt-contrib-concat": "~0.3.0",
-		    "grunt-contrib-copy": "^0.6.0",
-		    "grunt-contrib-cssmin": "~0.6.0",
-		    "grunt-contrib-imagemin": "^0.9.2",
-		    "grunt-contrib-uglify": "~0.2.7",
-		    "grunt-contrib-watch": "^0.6.1",
-		    "grunt-newer": "^1.0.0",
-		    "grunt-sass": "^1.0.0",
-		    "matchdep": "^0.3.0",
-		    "node-sass": "^2.0.0-beta"
+            "grunt-contrib-clean": "~0.6.0",
+            "grunt-contrib-concat": "~0.3.0",
+            "grunt-contrib-copy": "^0.6.0",
+            "grunt-contrib-cssmin": "~0.6.0",
+            "grunt-contrib-imagemin": "^0.9.2",
+            "grunt-contrib-uglify": "~0.2.7",
+            "grunt-contrib-watch": "^0.6.1",
+            "grunt-newer": "^1.0.0",
+            "grunt-sass": "^1.0.0",
+            "matchdep": "^0.3.0",
+            "node-sass": "^2.0.0"
         };
 
         // Sanitize names where we need to for PHP/JS
-        props.name = props.title.replace( /\s+/g, '-' ).toLowerCase();
-        // Development prefix (i.e. to prefix PHP function names, variables)
-        props.prefix = props.prefix.replace('/[^a-z_]/i', '').toLowerCase();
+        props.name              = props.title.replace( /\s+/g, '-' ).toLowerCase();
 
-        props.text_domain = props.text_domain.replace('/[^a-z_]/i', '').toLowerCase();
+        // Development prefix (i.e. to prefix PHP function names, variables)
+        props.prefix            = props.prefix.replace('/[^a-z_]/i', '').toLowerCase();
+
+        // Text Domain for localization
+        props.text_domain       = props.text_domain.replace('/[^a-z_]/i', '').toLowerCase();
 
         // Development prefix in all caps (e.g. for constants)
-        props.prefix_caps = props.prefix.toUpperCase();
-        // An additional value, safe to use as a JavaScript identifier.
-        props.js_name = props.php_class_name.replace( /\s+/g, '-' ).toLowerCase();
-        props.js_safe_name = props.js_name.replace(/[\W_]+/g, '_').replace(/^(\d)/, '_$1');
-        props.js_object_name = props.php_class_name.replace('/[^a-z_]/i', '').toLowerCase();
-        // An additional value that won't conflict with NodeUnit unit tests.
-        props.js_test_safe_name = props.js_safe_name === 'test' ? 'myTest' : props.js_safe_name;
-        props.js_safe_name_caps = props.js_safe_name.toUpperCase();
+        props.prefix_caps       = props.prefix.toUpperCase();
 
-        // Template-specific notes to be displayed after question prompts.
-        exports.after = 'You should now _cd wp-content/themes/' + props.js_safe_name +
-        '_ and install project dependencies with _npm ' +
-        'install && bower install_.  After that, you may execute project tasks with _grunt_. For ' +
-        'more information about installing and configuring Grunt, please see ' +
-        'the Getting Started guide:' +
-        '\n\n' +
-        'http://gruntjs.com/getting-started';
+        // An additional value, safe to use as a JavaScript identifier.
+        props.js_name           = props.php_class_name.replace( /\s+/g, '-' ).toLowerCase();
+        props.js_safe_name      = props.js_name.replace(/[\W_]+/g, '_').replace(/^(\d)/, '_$1');
+        props.js_object_name    = props.php_class_name.replace('/[^a-z_]/i', '').toLowerCase();
+
+        // An additional value that won't conflict with NodeUnit unit tests.
+        props.js_test_safe_name = props.js_safe_name === 'hatch' ? 'MyHatch' : props.js_safe_name;
+        props.js_safe_name_caps = props.js_safe_name.toUpperCase();
 
         // Files to copy and process
         var files = init.filesToCopy( props );
 
-        console.log( files );
+        var base_path = 'wp-content/themes/'; // define our default base path
+
+        if( 'N' == props.create_base_directories.toUpperCase() ) {
+            base_path = '';
+        }
+
+        base_path = '/' + base_path; // Add a slash prefix to our base path.
 
         // Actually copy and process files
         init.copyAndProcess( files, props );
 
-        var fs = require('fs');
-            fs.rename(init.destpath() + '/wp-content/themes/theme/js/theme/', init.destpath() + '/wp-content/themes/theme/js/'+ props.js_safe_name + '/');
-            fs.rename(init.destpath() + '/wp-content/themes/theme/', init.destpath() + '/wp-content/themes/'+ props.js_safe_name + '/');
+        if( 'N' !== props.create_base_directories.toUpperCase() ) {
+            // Create directories if needed.
+            grunt.file.mkdir('wp-content');
+            grunt.file.mkdir('wp-content/themes');
+
+            var src_path = init.destpath() + '/theme/';
+            var dest_path = init.destpath() + base_path + props.js_safe_name + '/';
+
+            // Copy Our folder to the new sub directory.
+            grunt.file.recurse( src_path, function( abspath, rootdir, subdir, filename ) {
+
+                if ( subdir == undefined ) {
+                    var dest = dest_path + filename;
+                } else {
+                    var dest = dest_path + '/' + subdir + '/' + filename;
+                }
+
+                grunt.file.copy( abspath, dest );
+            });
+
+            grunt.file.delete( src_path );
+
+        }
 
         // Generate package.json file
-        init.writePackageJSON( 'wp-content/themes/'+ props.js_safe_name + '/package.json', props );
+        init.writePackageJSON( base_path + 'theme/package.json', props );
+
+        var fs = require('fs');
+        fs.rename( init.destpath() + base_path + 'theme/js/theme/', init.destpath() + base_path + '/theme/js/'+ props.js_safe_name + '/'); // Rename our javascript directory
+        fs.rename( init.destpath() + base_path + 'theme/', init.destpath() + base_path + props.js_safe_name + '/'); // Rename our base directory
 
         // Done!
         done();
